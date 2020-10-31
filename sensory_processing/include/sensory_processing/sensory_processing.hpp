8b187d55c35f5f1533ea93468930355f17183570
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2020-10-31 21:54:01 +0100

    Added curiosity measuring method - TODO: take into account also which is currently done

--- sensory_processing/include/sensory_processing/sensory_processing.hpp
+++ sensory_processing/include/sensory_processing/sensory_processing.hpp
@@ -4,6 +4,10 @@
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
 #include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+
+#include <opencv2/opencv.hpp>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -24,11 +28,17 @@ namespace ml
         void ProcessSensoryInputs();
 
     private:
-        float prev_left_wheel_joint_angle_;
-        float prev_right_wheel_joint_angle_;
+        float _prev_left_wheel_joint_angle;
+        float _prev_right_wheel_joint_angle;
+
+        sensor_msgs::ImageConstPtr _prev_image;
 
         bool _WheelsStateChanged();
         bool _CloseToObstacle();
+        bool _Curiosity();
+
+        ros::Publisher _pains_pub;
+        commons::PainsPtr _pains;
 
         constexpr static float RANGE_THRESHOLD = 0.2F;
 
--- sensory_processing/src/sensory_processing.cpp
+++ sensory_processing/src/sensory_processing.cpp
@@ -3,8 +3,12 @@
 namespace ml
 {
     SensoryProcessing::SensoryProcessing()
-        : prev_left_wheel_joint_angle_(0), prev_right_wheel_joint_angle_(0)
+        : _prev_left_wheel_joint_angle(0), _prev_right_wheel_joint_angle(0)
     {
+        ros::NodeHandle nh("~");
+        _pains_pub = nh.advertise<commons::Pains>(commons::Topics::pains, 1, true);
+        _pains = boost::make_shared<commons::Pains>();
+        _prev_image = boost::make_shared<sensor_msgs::Image>();
     }
 
     SensoryProcessing::~SensoryProcessing()
@@ -13,15 +17,43 @@ namespace ml
 
     void SensoryProcessing::ProcessSensoryInputs()
     {
-        if (_WheelsStateChanged())
+        // Low battery level pain
+        auto start = commons::now();
+        bool wheels_state_changed = _WheelsStateChanged();
+        ROS_DEBUG_STREAM("_WheelsStateChanged: " << commons::elapsed(commons::now(), start) << " ms");
+
+        if (wheels_state_changed)
         {
             ROS_DEBUG("Wheels state changed. Pain of low battery level increases.");
+            _pains->low_battery_level += 0.01;
         }
 
-        if (_CloseToObstacle())
+        // Mechanical damage pain
+        start = commons::now();
+        bool close_to_obstacle = _CloseToObstacle();
+        ROS_DEBUG_STREAM("_CloseToObstacle: " << commons::elapsed(commons::now(), start) << " ms");
+
+        if (close_to_obstacle)
         {
             ROS_DEBUG("Robot is too close to some obstacle e.g. wall. Pain of mechanical damage increases.");
+            _pains->mechanical_damage += 0.01;
         }
+
+        // Curiosity
+        start = commons::now();
+        bool curiosity = _Curiosity();
+        ROS_DEBUG_STREAM("_Curiosity: " << commons::elapsed(commons::now(), start) << " ms");
+
+        if (curiosity)
+        {
+            ROS_DEBUG("Something with curiosity happened. Do something boiii.");
+            _pains->curiosity += 0.01;
+        }
+
+        _pains->header.seq++;
+        _pains->header.stamp = ros::Time::now();
+
+        _pains_pub.publish(_pains);
     }
 
     bool SensoryProcessing::_WheelsStateChanged()
@@ -46,11 +78,11 @@ namespace ml
         float right_wheel_joint_angle = wheels_state.position[right_wheel_joint_name_it - wheels_state.name.begin()];
         // ROS_DEBUG_STREAM("\nl: " << left_wheel_joint_angle << "\nr: " << right_wheel_joint_angle);
 
-        bool left_changed = std::fabs(left_wheel_joint_angle - prev_left_wheel_joint_angle_) > 0.01;
-        bool right_changed = std::fabs(right_wheel_joint_angle - prev_right_wheel_joint_angle_) > 0.01;
+        bool left_changed = std::fabs(left_wheel_joint_angle - _prev_left_wheel_joint_angle) > 0.01;
+        bool right_changed = std::fabs(right_wheel_joint_angle - _prev_right_wheel_joint_angle) > 0.01;
 
-        prev_left_wheel_joint_angle_ = left_wheel_joint_angle;
-        prev_right_wheel_joint_angle_ = right_wheel_joint_angle;
+        _prev_left_wheel_joint_angle = left_wheel_joint_angle;
+        _prev_right_wheel_joint_angle = right_wheel_joint_angle;
 
         return left_changed || right_changed;
     }
@@ -69,6 +101,23 @@ namespace ml
             }
         }
         return too_close;
+    }
+
+    bool SensoryProcessing::_Curiosity()
+    {
+        sensor_msgs::ImageConstPtr rgb = ros::topic::waitForMessage<sensor_msgs::Image>("/camera/rgb/image_raw");
+
+        bool img_diff = (rgb->data != _prev_image->data);
+
+        // auto cv_ptr = cv_bridge::toCvShare(rgb);
+        // cv::Mat t;
+        // cv::cvtColor(cv_ptr->image, t, cv::COLOR_BGR2GRAY);
+
+        // cv::imshow("rgb", cv_ptr->image);
+        // cv::waitKey(1);
+
+        _prev_image = rgb;
+        return img_diff;
     }
 
 }; // namespace ml
