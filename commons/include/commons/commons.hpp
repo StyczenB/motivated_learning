69e1188f2d12a1303cc1d7e1e92ef2e1d89245bc
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2020-10-31 21:53:00 +0100

    Added time measuring method

--- commons/include/commons/commons.hpp
+++ commons/include/commons/commons.hpp
@@ -1,14 +1,32 @@
 #ifndef COMMONS_HPP
 #define COMMONS_HPP
 
+#include <chrono>
 #include <string>
 
 namespace commons
 {
+    using clock = std::chrono::system_clock;
+    static std::chrono::time_point<std::chrono::system_clock> now()
+    {
+        return clock::now();
+    }
+
+    static double elapsed(std::chrono::time_point<std::chrono::system_clock> stop, std::chrono::time_point<std::chrono::system_clock> start)
+    {
+        return std::chrono::duration<double, std::milli>(stop - start).count();
+    }
+
     struct Topics
     {
-        inline static std::string pains = "pains";
+        inline static std::string pains = "/pains";
     };
+
+    // struct Frames
+    // {
+    //     inline static std::string robot = "robot";
+    // };
+
 } // namespace commons
 
 #endif
